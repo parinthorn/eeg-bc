e5e54578775bf2364ba6283e54a4140e7da8cb38
parinthorn <30713778+parinthorn@users.noreply.github.com>
2020-08-10 11:06:17 +0700

    Add files via upload

--- Source_selection/constr_LS_eeg.m
+++ Source_selection/constr_LS_eeg.m
@@ -2,17 +2,34 @@ function [C,flag] = constr_LS_eeg(V,L,W,C_in)
 % This program solve constrained least square (flag=0)
 %       min_C ||V-LCW||_F
 %       subject to C(C_in==0)==0
-% If multiple solution exists, minimum norm solution is returned instead (flag=-1)
+% If Constrained Least square is well-defined, flag = 0
+% If Constrained LS is not well-defined, return regularized constrained LS
+% solution flag = -1.
 [m,n] = size(C_in);
 C= zeros(m,n);
 nz_ind = find(norms(C_in,2,2));
 Lnz = L(:,nz_ind);
 if length(nz_ind)>= size(L,1)
-    C(nz_ind,:) = Lnz'*(Lnz*Lnz'\V/W); %minimum norm solution, when solution is not unique
+    %     C(nz_ind,:) = Lnz'*(Lnz*Lnz'\V/W); %minimum norm solution, when solution is not unique
+    A = Lnz'*Lnz;%experimental features
+    invWWt = (W*W')\eye(n);
+    B = 0.01*(invWWt);
+    F = Lnz'*V*W'*invWWt;
+    C(nz_ind,:) = sylvester(A,B,F);
+    %     C(nz_ind,:) = (tmp+0.01*eye(size(tmp,1)))\(Lnz'*V*W')/(W*W'); %experimental features
     flag = -1;
 else
-    C(nz_ind,:) = (Lnz'*Lnz)\(Lnz'*V*W')/(W*W'); % well-defined least square case, solve normal eq
+    A = Lnz'*Lnz;
+    C(nz_ind,:) = (A)\(Lnz'*V*W')/(W*W'); % well-defined least square case, solve normal eq
     flag = 0;
+    if rcond(A)<eps % machine precision
+        A = Lnz'*Lnz;%experimental features
+        invWWt = (W*W')\eye(n);
+        B = 0.01*(invWWt);
+        F = L'*V*W'*invWWt;
+        C(nz_ind,:) = sylvester(A,B,F);
+        flag = -2;
+    end
 end
 
 
