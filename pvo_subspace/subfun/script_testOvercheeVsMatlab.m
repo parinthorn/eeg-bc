289f15d61b89383ab9ff85cbf1af5c31de8a7076
jitkomut <jitkomut.s@chula.ac.th>
2020-10-07 18:25:52 +0700

    test uploading 3

--- pvo_subspace/subfun/script_testOvercheeVsMatlab.m
+++
@@ -1,8 +0,0 @@
-load iddata3
-
-[A,B,C,D,K,R] = subid(z3.y,z3.u,10);
-sys_overchee = ss(A,B,C,D,z3.Ts);
-n4opt =  n4sidOptions('N4Weight','CVA');
-sys_matlab = n4sid(z3,1:5,n4opt);
-
-compare(z3,sys_overchee,sys_matlab);
